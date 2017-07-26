c399a63ac7100d9d7e51207a5943a230cf914908
Sebastian C <crossasebastian@gmail.com>
2017-07-26 11:27:47 -0600

    Logo now centered programatically

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Centers the swoosh logo and positions it where we want it to be
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 10, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        // Image will always cover the view
+        bgImg.frame = view.frame
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -25,4 +28,3 @@ class ViewController: UIViewController {
     }
 
 }
-
