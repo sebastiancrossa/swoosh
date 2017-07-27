8ee411c34a6991380c08859409ac07e46d350bac
Sebastian C <crossasebastian@gmail.com>
2017-07-27 11:18:58 -0600

    Added simple button functionality on second screen

--- Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Controller/LeagueVC.swift
@@ -10,18 +10,42 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    // Declaring a variable from Player
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Initializing the struct
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    // Used for the above
+    func selectLeague(leagueType: String) {
+        player.desiredLeage = leagueType
+        nextBtn.isEnabled = true
     }
     
     /*
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Sebastian Crossa on 7/27/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeage: String?
+    var selectedSkillLevel: String?
+}
