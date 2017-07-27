c400a1ef248d173816c8606654082bcd31425f37
Sebastian C <crossasebastian@gmail.com>
2017-07-27 10:35:09 -0600

    Created VC file for each screen. Minimal refactoring

---
+++ Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,38 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Sebastian Crossa on 7/27/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .light
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swoosh/Controller/SkillVC.swift
@@ -0,0 +1,33 @@
+//
+//  SkillVC.swift
+//  Swoosh
+//
+//  Created by Sebastian Crossa on 7/27/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .light
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
